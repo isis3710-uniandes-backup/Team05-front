e5ec1be8c8174fdd2cb14d7a4b74c2a8df4efcee
cpotdevin <chrpotdevin@gmail.com>
2019-04-14 12:59:19 -0500

    remove hamburger menu

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import MobileNavbar from './MobileNavbar';
-import MenuItems from './MenuItems';
 import './Navbar.css';
 
-export default class Navbar extends React.Component {
-  render() {
-    return (
-      <nav className='navbar'>
-        <div className='navbar-content'>
-          <div className='navbar-desktop'>
-            <h1 className='navbar-banner'>
-              <NavLink exact to='/'>bioAndes</NavLink>
-            </h1>
-            <MenuItems />
-          </div>
+const Navbar = () => (
+  <nav className='navbar'>
+    <div className='navbar-content'>
+      <h1 className='navbar-banner'>
+        <NavLink exact to='/'>bioAndes</NavLink>
+      </h1>
+      <ul className='navbar-items'>
+        <li className='navbar-item'>
+          <NavLink exact to='/'>Inicio</NavLink>
+        </li>
+        <li className='navbar-item'>
+          <NavLink exact to='/clases'>Clases</NavLink>
+        </li>
+        <li className='navbar-item'>
+          <NavLink exact to='/nosotros'>Nosotros</NavLink>
+        </li>
+        <li className='navbar-item'>
+          <NavLink exact to='/contactenos'>Contáctenos</NavLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
 
-          <div className='navbar-mobile'>
-            <MobileNavbar />
-          </div>
-        </div>
-      </nav>
-    );
-  }
-}
+export default Navbar;
