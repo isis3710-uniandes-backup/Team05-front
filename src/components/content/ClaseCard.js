4c41f42cd211b5808c9c11c92eccfa6d1c6106d3
cpotdevin <chrpotdevin@gmail.com>
2019-04-18 18:01:08 -0500

    refactor card styles

--- src/components/content/ClaseCard.js
+++ src/components/content/ClaseCard.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Card.css';
 
-const ClaseCard = ({ redirect, url, clase, imagen }) => (
-  <div className='card' onMouseDown={() => redirect(`${url}?clase=${clase}`)}>
-    <img src={imagen} alt={clase} />
-    <h2><a href={`${url}?clase=${clase}`}>{clase}</a></h2>
-  </div>
-);
+const ClaseCard = ({ redirect, url, clase, imagen }) => {
+  const imageStyle = {
+    backgroundImage: `url(${imagen})`
+  };
+
+  return (
+    <div className='card' onClick={() => redirect(`${url}?clase=${clase}`)}>
+      <div style={imageStyle} />
+      <h2><a href={`${url}?clase=${clase}`}>{clase}</a></h2>
+    </div>
+  );
+};
 
 export default ClaseCard;
