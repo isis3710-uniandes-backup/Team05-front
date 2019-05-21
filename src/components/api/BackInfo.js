a6419e785b531df3adb54a7058fd3b753531d939
cpotdevin <chrpotdevin@gmail.com>
2019-05-20 19:33:00 -0500

    remove console logs

--- src/components/api/BackInfo.js
+++ src/components/api/BackInfo.js
@@ -1,10 +1,7 @@
 export function getBackUrl() {
-  console.log(process.env.NODE_ENV);
   if (process.env.NODE_ENV === 'production') {
-    console.log('hola');
     return 'https://boiling-brushlands-27343.herokuapp.com';
   } else {
-    console.log('hola2');
     return 'http://localhost:3001';
   }
 }
